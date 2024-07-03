be9299d1799df936b8d56a3efdd7a77428f72f81
Prashant Kumar <161224029+pkprashantkr@users.noreply.github.com>
2024-07-03 13:24:38 +0000

    modified

--- Basics01/02_datatypes.js
+++ Basics01/02_datatypes.js
@@ -33,6 +33,7 @@ console.log(typeof state);      // Its datatype is undefined
 // TYPE CONVERSIONS
 
 console.log("----------------------------");
+console.log("String to number");
 
 let score = "33"
 console.log("Score is in " + typeof score);
@@ -43,13 +44,67 @@ console.log("Score is in " + typeof scoreInNumber);
 console.log("Value of Score is " + score);
 
 
+console.log("----------------------------");
+console.log("String to NaN");
 
 let score2 = "33abc"
-console.log("Score is in " + typeof score2);
+console.log("Score 2 is in " + typeof score2);
 
 let score2InNumber = Number(score2)
-console.log("Score is in " + typeof score2InNumber);
-console.log("Value of Score2 is " + score2InNumber);
+console.log("Value of Score 2 is " + score2InNumber);        //value will be NaN which is Not a Number
+
+
+console.log("----------------------------");
+console.log("null to number");
+
+let score3 = null
+console.log("Score 3 is in " + typeof score3);
+
+let score3InNumber = Number(score3)
+console.log("Value of Score 3 is " + score3InNumber);
+
+
+console.log("----------------------------");
+console.log("Boolean true to number");
+
+let score4 = true
+console.log("Score 4 is in " + typeof score4);
+
+let score4InNumber = Number(score4)
+console.log("Value of Score 4 is " + score4InNumber);
+
+
+console.log("----------------------------");
+console.log("Boolean false to number");
+
+let score5 = false
+console.log("Score 5 is in " + typeof score5);
+
+let score5InNumber = Number(score5)
+console.log("Value of Score 5 is " + score5InNumber);
+
+
+console.log("----------------------------");
+console.log("undefined to NaN");
+
+let score6 = undefined
+console.log("Score 6 is in " + typeof score6);
+
+let score6InNumber = Number(score6)
+console.log("Value of Score 6 is " + score6InNumber);
+
+
+/*
+    Soo, It shows that.:
+        "33" => 33
+        "33abc" => NaN
+        true => 1
+        1 => true
+        false => 0
+        0 => false
+        null => 0
+        undefined => NaN
+*/
 
 
 
