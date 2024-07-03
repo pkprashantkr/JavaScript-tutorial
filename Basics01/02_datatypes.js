a7180ecf7de10ac6836fe2b24d8498813dd49753
Prashant Kumar <161224029+pkprashantkr@users.noreply.github.com>
2024-07-03 08:14:49 +0000

    modified

---
+++ Basics01/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict";       // Treats all Js code as newer version.
+
+
+/*
+    number: 1234, upto 2^53.
+    bigInt: above 2^53
+    string : "Name"
+    boolean : true/false
+    null : standalone value or empty
+    undefined : not assigned
+    symbol : for uniqueness
+    object
+*/
+
+let name = "Prashant"
+console.log(typeof name);
+
+let age = 13;
+console.log(typeof age);
+
+let isLoggedIn = false
+console.log(typeof isLoggedIn);
+
+let box = null
+console.log(typeof box);        // Its datatype is object
+
+let state
+console.log(typeof state);      // Its datatype is undefined
+
+
+
+
+
+
+// ECMA stands for European Computer Manufacturers Associatio which is a Script for JS Standards
+
+
+// alert(4+3)       //This will cause error becoz we are using nodejs and not browser.
+
