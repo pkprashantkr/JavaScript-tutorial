d6add9695e69dc7a32542b7bba28603874cee7b4
Prashant Kumar <161224029+pkprashantkr@users.noreply.github.com>
2024-07-04 16:25:38 +0000

    modify

--- Basics/06_maths.js
+++ Basics/06_maths.js
@@ -1,5 +1,20 @@
 // -----------------------Number-----------------------
 
+const score = 100
+console.log(score);
+
+const balance = new Number(298.6547)         // Will give additional functions or properties
+console.log(balance);
+
+console.log(balance.toString());        // Convert the number into String
+
+console.log(balance.toString().length);     // Now, it is a string and we can use string properties on it.
+
+console.log(balance.toFixed(3));        //  Rounds the string to a specified number of decimals.
+
+
+console.log(balance.toPrecision(3));
+
 
 
 
