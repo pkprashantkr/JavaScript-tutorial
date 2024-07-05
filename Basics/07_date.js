5f7f078fd1cd7be492e910306defe611867ecd60
Prashant Kumar <161224029+pkprashantkr@users.noreply.github.com>
2024-07-05 10:02:29 +0000

    modified

--- Basics/07_date.js
+++ Basics/07_date.js
@@ -31,5 +31,39 @@ console.log(specificDateTime.toLocaleString());
 
 // To declare date in specific format
 
-let specificFormat = new Date("01-12-2043")
+let specificFormat = new Date("01-12-2043")     // In this month starts from 1.
 console.log(specificFormat.toLocaleString());
+
+
+// Time stamp
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);                   // Time Stamp in milliseconds
+console.log(specificFormat.getTime());      // Time Stamp from a specific date
+
+console.log(Math.floor(Date.now() / 1000));     // Time Stamp in Seconds with floor value
+
+
+// Some Date Functions
+
+let newDate = new Date()
+console.log(newDate.getDate());
+console.log(newDate.getDay());          // Gives day like Monday, Tuesday, etc and it starts from 1.
+console.log(newDate.getFullYear());
+console.log(newDate.getHours());
+console.log(newDate.getMilliseconds());
+console.log(newDate.getMinutes());
+console.log(newDate.getMonth() + 1);        // Adding 1 so the months get started from 1
+console.log(newDate.getSeconds());
+console.log(newDate.getTime());
+
+
+// To give some customization to Date
+
+console.log(newDate.toLocaleDateString('default', {
+        month: "long",          // Type ctrl + space to get suggestions
+        weekday: "long",
+        day: "2-digit" 
+    })
+);
