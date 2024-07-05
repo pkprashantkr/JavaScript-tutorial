1c3c93aff5f9715799c8a8563aadcdaf0fc96dd1
Prashant Kumar <161224029+pkprashantkr@users.noreply.github.com>
2024-07-05 13:32:55 +0000

    modify

---
+++ Basics/08_array.js
@@ -0,0 +1,7 @@
+// --------------------Arrays-----------------------
+// Array is an object having collection of multiple items in single variable
+// It can a mix of multiple datatypes.
+// Arrays are re-sizable in js.
+
+
+const arr = [4, 3, 2, 6, true, "Prashant"]
