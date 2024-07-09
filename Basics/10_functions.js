3d5177432b54a8a04aeef243d515bc4fca88dd91
Prashant Kumar <161224029+pkprashantkr@users.noreply.github.com>
2024-07-09 08:50:52 +0000

    modified

--- Basics/10_functions.js
+++ Basics/10_functions.js
@@ -70,13 +70,90 @@ console.log("Sum: ", sum);                  // Here, the value of addition is ge
 
 
 
+// Check for the entered value by using if()
+
+function loginUserMessage(username) {
+    if(!username){                                      // Checking that valid username is entered other than undefined, null, etc.
+        console.log("Please enter a valid username");
+        return
+    }
+    return `${username} Just Logged in`
+}
+
+console.log(loginUserMessage("Prashant"));
+console.log(loginUserMessage());            // Passing no value, so it will give undefined.
+
+
+
+// For this above function, we can give a default value to the username by writing
+
+function loginUserMessage2(username = "John") {         // giving default value to username as John
+    if(!username){                                      
+        console.log("Please enter a valid username");
+        return
+    }
+    return `${username} Just Logged in`
+}
+
+console.log(loginUserMessage2());               // If no argument is passed then the default value will return
+console.log(loginUserMessage("Prashant"));
 
 
 
 
+// While passing the parameters, num1 and num2 are declared as parameters but when we call the function..
+function calculateCartPrice1(num1){
+    return num1
+}
+console.log(calculateCartPrice1(500, 644, 334, 584));       // Here, we are passing 4 arguments while only 1 was required. So, this will only give the value of 500 and 644.
+
+
+// To tackle this above situation, We can use Rest operator (...)
+// (...) this is also known as spread operator but its name is based on the usecase.
+// Now, in this below example we take num1 parameter with (...) 
+// which will make it flexible and put all the passed arguments in an array 
+
+function calculateCartPrice2(...num1){
+    return num1
+}
+console.log(calculateCartPrice2(500, 644, 334, 788, 643));      // Now, any number of arguments can be passed and all the values will be stored in an array.
+
+
+// Here,
+
+function calculateCartPrice3(val1, val2, ...num1){
+    return num1
+}
+console.log(calculateCartPrice3(500, 644, 334, 788, 643));      // Now, Only 334, 788 and 643 will get assigned in num 1, becoz 500 goes to val1 and 644 goes to val2.
 
 
 
+// Passing object in function
+
+const mainUser = {
+    username: "mainuser@gmail.com",
+    price: 322
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is Rs. ${anyobject.price}`);
+}
+handleObject(mainUser)              // Accessing object members by using function
+
+handleObject({                      // directly passing an object in the function
+    username: "direct@gmail.com",
+    price: 568
+})
 
 
 
+// Passing arrays in function
+
+const newArr = [200, 455, 674, 363]
+
+function returnSecondValue(getArr){
+    return getArr[1]
+}
+
+console.log(returnSecondValue(newArr));                     // Return 2nd value from newArr array
+console.log(returnSecondValue([344, 535, 256, 654]));       // Return 2nd value from this array
