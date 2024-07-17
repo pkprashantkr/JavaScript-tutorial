4b57cc368179d5567bff57d3fe5bffb676f48ba3
Prashant Kumar <161224029+pkprashantkr@users.noreply.github.com>
2024-07-17 20:22:06 +0000

    modified

---
+++ Basics/14_control_flow.js
@@ -0,0 +1,45 @@
+// =================== Control Statement =================================
+
+// 1) if Statement: It checks the condition inside the () then executes the code inside.
+//                  The condition should always be true to execute the inside code
+
+
+    // Equals to
+    if(2 == 2){
+        console.log("Equal");
+    }
+
+    // Less than
+    if(1 < 2){
+        console.log("Less than");
+    }
+
+    // Greater than
+    if(4 > 2){
+        console.log("Greater than");
+    }
+
+    // Less than or equal to
+    if(2 <= 2){
+        console.log("Less than or equal to");
+    }
+
+    // Greater than or equal to
+    if(2 >= 2){
+        console.log("Greater than or equal to");
+    }
+
+    // not equal to
+    if(5 != 2){
+        console.log("not equal to");
+    }
+
+    // strict quality operator (===): Checks for datatype also.
+    if(2 === "2"){
+        console.log("Strict quality operator");
+    }
+
+    if(2 !== "2"){
+        console.log("Strict quality operator with not");
+    }
+    
