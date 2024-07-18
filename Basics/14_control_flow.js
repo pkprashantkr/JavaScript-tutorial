1bbc47fc101ba2c6f245e885551b50327c44a008
Prashant Kumar <161224029+pkprashantkr@users.noreply.github.com>
2024-07-18 06:58:28 +0000

    modified

--- Basics/14_control_flow.js
+++ Basics/14_control_flow.js
@@ -43,3 +43,104 @@
         console.log("Strict quality operator with not");
     }
     
+
+// 2) if else Statement
+
+    const temp = 56
+
+    if(temp < 50){
+        console.log("Temperature is less than 50");
+    } else{
+        console.log("Temperature is greater than 50");
+    }
+
+
+// 3) if else if Statement: Check multiple conditions
+
+    const balance = 1200
+
+    if(balance < 500){
+        console.log("Less than 500");
+
+    } else if(balance < 700){
+        console.log("Less than 700");
+
+    } else if(balance < 900){
+        console.log("Less than 900");
+
+    } else if(balance < 1200){
+        console.log("Less than 1200");
+    } else{
+        console.log("Not valid");
+    }
+
+    // 4) Using Logical Operators
+
+    const isLoggedIn = true
+    const debitCard = true
+    const isLoggedInFromGoogle = true
+    const isLoggedInFromEmail = false
+
+    // Logical AND (&&) operator: Returns true if both conditions are true
+    if(isLoggedIn && debitCard){
+        console.log("Shopping done.....!");
+    }
+
+    // Logical OR (||) operator: Returns true if one of the conditions is true
+    if(isLoggedInFromEmail || isLoggedInFromGoogle){
+        console.log("User Logged in successfully.......!");
+    }
+
+
+
+// 4) Switch Statement: Can check multiple cases
+
+    const month = 7
+
+    switch (month) {
+        case 1:
+                console.log("January");
+            break;
+        case 2:
+                console.log("February");
+            break;
+        case 3:
+                console.log("March");
+            break;
+        case 4:
+                console.log("April");
+            break;
+        case 5:
+                console.log("May");
+            break;
+        case 6:
+                console.log("June");
+            break;
+        case 7:
+                console.log("July");
+            break;
+        case 8:
+                console.log("August");
+            break;
+        case 9:
+                console.log("September");
+            break;
+        case 10:
+                console.log("October");
+            break;
+        case 11:
+                console.log("November");
+            break;
+        case 12:
+                console.log("December");
+            break;
+    
+        default:
+                console.log("Invalid");
+            break;
+    }
+
+
+
+
+
