10082088698fc3ce09183d9f6b4aa9174ec9b4d9
Prashant Kumar <161224029+pkprashantkr@users.noreply.github.com>
2024-07-25 12:56:44 +0530

    modified

--- Basics/01_variables.js
+++ Basics/01_variables.js
@@ -1,5 +1,13 @@
 // Whether JS is a typesafe language means variables can be declared without using any of these below but it is not a good practice.
 
+// Hoisting: In JS, Variable and functions are hoisted which means there declaration is moved on the top of code.
+// Hoisting refers to the built-in behavior of the language through which declarations of functions, variables, and classes are moved to the top of their scope – all before code execution. 
+// In turn, this allows us to use functions, variables, and classes before they are declared.
+// Ex:
+    console.log(a);         // This will not give error as other languages do, but in js this will give undefined.
+    var a = 12;
+
+
 // There are 3 types of variables in JS
 
 // 1) var : It is not used nowadays because of the issue in block scope & functional scope. It can be modified same as let but also outside this file.
